439c5917deeefa182d82da93db3c4f0db52fd3cd
sn0am <81282692+sn0am@users.noreply.github.com>
2023-04-09 23:39:05 -0700

    Update SFTP_upload.py

--- SFTP_upload.py
+++ SFTP_upload.py
@@ -5,6 +5,6 @@ local_folder = ''  # Address for local folder.
 remote_root_dir = ''  # Address for root SFTP folder.
 remote_folder = 'Misc'  # Name of remote folder to store all local folder root files.
 
-recursive_upload(remote_root_dir, remote_folder, local_folder)
+recursive_upload(remote_root_dir, remote_folder, local_folder)  #  Upload all files from local folder.
 
-time.sleep(10)
+time.sleep(10)  # Sleep timer added so console output can be read.
