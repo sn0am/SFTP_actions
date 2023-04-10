ab2d33deca7f64a2bcf62051c100c929eb5db2f5
sn0am <81282692+sn0am@users.noreply.github.com>
2023-04-09 23:37:56 -0700

    Update SFTP_download.py

--- SFTP_download.py
+++ SFTP_download.py
@@ -4,10 +4,10 @@ import time
 remote_root_dir = ''  # Address for root SFTP folder.
 local_folder = ''  # Address for local folder.
 
-remote_root_download(remote_root_dir, local_folder)
+remote_root_download(remote_root_dir, local_folder)  # Only download files placed in the remote root directory.
 
-recursive_download(remote_root_dir, remote_root_dir, local_folder)
+recursive_download(remote_root_dir, remote_root_dir, local_folder)  # Download all files within folders and subfolders within the remote root directory.
 
 print("No new files available to download from the remote subdirectories.")
 
-time.sleep(10)
+time.sleep(10)  # Sleep timer added so console output can be read.
